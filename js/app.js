7e0dbd42b193b271d383befc14580a189dfc020a
Fahim <fhasan5@wgu.edu>
2023-10-24 21:20:38 -0500

    colorie app tracker

--- js/app.js
+++ js/app.js
@@ -1,8 +1,62 @@
-class ColorTracler {
+class CalorieTracker {
   constructor() {
     this._calorieLimit = 2000;
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
+
+    this._displayCaloriesTotal();
+  }
+
+  //public API
+  addMeal(meal) {
+    this._meals.push(meal);
+    this._totalCalories += meal.calories;
+    this._render();
+  }
+
+  addWorkout(workout) {
+    this._workouts.push(workout);
+    this._totalCalories -= workout.calories;
+    this._render();
+  }
+
+  //Private API/methods
+  _displayCaloriesTotal() {
+    const calorieTotoalEl = document.getElementById('calories-total');
+    calorieTotoalEl.innerHTML = this._totalCalories;
+  }
+
+  //render
+  _render() {
+    this._displayCaloriesTotal();
   }
 }
+
+class Meal {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+class Workout {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+
+const tracker = new CalorieTracker();
+const breakfast = new Meal('Breakfast', 400);
+const lunch = new Meal('Lunch', 350);
+tracker.addMeal(breakfast);
+tracker.addMeal(lunch);
+
+const run = new Workout('Morning Run', 300);
+tracker.addWorkout(run);
+
+console.log(tracker._meals);
+console.log(tracker._workouts);
+console.log(tracker._totalCalories);
